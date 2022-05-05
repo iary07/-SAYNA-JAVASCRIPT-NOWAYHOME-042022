3f727e0544d29f7d7a128b0620fa1710a347daf8
iary07 <99659892+iary07@users.noreply.github.com>
2022-05-05 17:26:49 +0300

    Add files via upload

---
+++ projet.js/SAYNA-HTMLCSS-NOWAYHOME-032022-main/Script.js/index.js
@@ -0,0 +1,201 @@
+// Set the date we're counting down to
+var countDownDate = new Date("May 04, 2022 08:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("spider").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("spider").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
+const send = document.getElementById('boss');
+const para = document.getElementById('man');
+const border = document.getElementById('pup');
+const btn = document.querySelector('.me');
+
+const inputs = document.getElementsByClassName('aa');
+
+send.addEventListener('submit', function(e) {
+    e.preventDefault();
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == '') {
+            border.style.display = 'block'
+            para.innerText = 'remplir votre formulaire'
+            return false
+        } else {
+            border.style.display = 'block'
+            para.innerText = 'votre formulaire est accédé'
+        }
+
+    }
+
+})
+
+btn.addEventListener('click', () => {
+
+    border.style.display = 'none'
+
+
+
+})
+
+let coeur = document.getElementById('coeur');
+coeur.addEventListener('click', myFav);
+
+function myFav() {
+    if (coeur.innerHTML == '<i class="fa-regular fa-heart"></i>') {
+        coeur.innerHTML = '<i class="fa-solid fa-heart">'
+    } else { coeur.innerHTML = '<i class="fa-regular fa-heart"></i>' }
+
+}
+
+$(document).ready(function() {
+    $('.button2').on({
+        mouseenter: function() {
+            $(this).css({
+                'background-color': '#ffffff',
+                'color': 'red',
+                'box-shadow': '0 0 30px 0 #ffffff'
+            })
+
+        },
+        mouseleave: function() {
+            $(this).css({
+                'background-color': 'transparent',
+                'color': '#ffffff',
+                'box-shadow': '0 0 0 0'
+            })
+
+        }
+    })
+
+    $('.button1').on({
+        mouseenter: function() {
+            $(this).css({
+                'background-color': 'transparent',
+                'color': '#ffffff'
+            })
+
+        },
+        mouseleave: function() {
+            $(this).css({
+                'background-color': '#ffffff',
+                'color': 'red',
+                'border': '1px solid #ffffff'
+            })
+
+        }
+    })
+
+    $('.mediapx').on({
+        mouseenter: function() {
+            $(this).css({
+
+                'color': '#ffffff',
+            })
+
+        },
+        mouseleave: function() {
+            $(this).css(
+                'color', '#000000',
+            )
+
+        }
+    })
+
+    $('.card-text').hide();
+    $('.card-text').nextUntil('.button1').hide()
+
+
+    $('.img1').on({
+        mouseenter: function() {
+            $('.card1').fadeTo(4000, 0.8)
+        },
+        mouseleave: function() {
+            $('.card1').hide(1000)
+        }
+    })
+
+
+
+    $('.img2').on({
+        mouseenter: function() {
+            $('.card2').fadeTo(4000, 0.8)
+        },
+        mouseleave: function() {
+            $('.card2').hide(1000)
+        }
+    })
+
+
+
+    $('.img3').on({
+        mouseenter: function() {
+            $('.card3').fadeTo(4000, 0.8)
+            $('.card-text').nextUntil('.button1').fadeTo(4000, 0.8)
+        },
+        mouseleave: function() {
+            $('.card3').hide(1000)
+            $('.card-text').nextUntil('.button1').hide()
+        }
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
